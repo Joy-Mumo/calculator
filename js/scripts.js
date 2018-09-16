8ba5fc0081fa1ea8fd899ad8bd9d372e16ca2eab
Joy Vivika <joiemumo@gmail.com>
2018-09-16 11:32:11 +0300

    linked js to html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var input1=parseInt(prompt("enter value"));
+var input2=parseInt(prompt("enter your value2"));
+var input3=parseInt(prompt("enter your value3"));
+var combo=alert(input1+input2+input3) ;{
+return input1 +input2 +input
+}
